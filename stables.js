935285b876dc1ebf1227d92248d6d13a64b06187
Leana <leanale003@gmail.com>
2024-09-19 15:42:15 -0700

    initialize stables and add a horse

---
+++ stables.js
@@ -0,0 +1,27 @@
+
+console.log("horse! horse! horse! horse! horse!horse!horse!horse!horse!horse!")
+
+/* 
+name variables using CAMEL CASE 
+A way to separate the words 
+First letter of each word capitalized no spaces
+*/
+
+let horseName = "SomeCoolGuy";          //this is a string, for string prefer double quotes " "
+horseName = "Chicken";
+let horseAge = 5;
+
+console.log(horseName);
+console.log(horseAge);
+
+let isHorseInStable = false;            // always name boolean in a way thats readable to people
+
+console.log(isHorseInStable);
+
+isHorseInStable = false;
+horseName = "Hippo"                     // still chicken ^ because it already console.log out before this line 
+
+console.log(horseName);
+
+
+
