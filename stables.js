4263c62743da1aca872b907a7ed439437f90524a
Leana <leanale003@gmail.com>
2024-10-10 15:13:16 -0700

    add lesson 6 code and some lab 5

--- stables.js
+++ stables.js
@@ -24,6 +24,7 @@ let stableIntroduction = `Welcome to "${horseName}"'s stable`;
 
 let visitorHorseName = "Chicken";
 
+/*
 if (horseName === "Hippo" && !isHorseInside) {
     console.log (`${horseName} is outside!`);
 } else if (horseName === "Hippo" && isHorseInside) {
@@ -31,9 +32,56 @@ if (horseName === "Hippo" && !isHorseInside) {
 } else {
     console.log ("Where is my horse!?")
 }
+*/
+
+//--------  LESSON-6 CODE  --------
+
+//Horse = ["name", age, isInside, "nickname"];
+let horseOne = [horseName, horseAge, isHorseInside, horseNickName];
+let horseTwo = ["Cream of Mushroom", 2, false, "Soup"];
+let horseThree = ["Roast", 5, ]
+
+let horseNames = [horseName, horseTwo[0], "Roast"];
+let horseAges = [horseAge, 2, 10];
+let horseNickNames = [horseNickName, "Soup", "Beef"];
+let areHorsesInside = [isHorseInside, false, true];
+
+console.log(horseOne[0] + "'s nickname is " + horseOne[3]);
+horseOne.push("Blue"); //pushes up something to the end of an array
+console.log(`${horseTwo[0]}'s nickname is ${horseTwo[3]}`);
+console.log(horseOne[1] + horseTwo[1]);
+
+if (horseOne[1] === 3){
+    console.log(horseOne[0] + " is 3");
+} else {
+    console.log(horseOne[0] + " is NOT 3");
+}
+
+let horseIntro = `There are ${horseNames.length} horses staying at my stables: ${horseNames[0]}, ${horseNames[1]}, and ${horseNames[2]}! ${horseNames[0]}'s nickname is ${horseNickNames[0]} and his friends go by ${horseNickName[1]} ${horseNickNames[3]}, and ${horse}`
+console.log(horseIntro);
 
 
 
+
+
+
+
+
+
+/* 
+
+LAB TODAY - branch lesson-6 donr by Sunday
+
+Stables.js
+-output an introduction to our horses
+- output whether your horses are inside or outside (at least 3!!!)
+
+ReadMe
+3 confusing things 
+3 things you feel like you have a solid understanding of 
+Any analogies that you use to think about things (gif?)
+
+*/
 
 
 
