4245700706f41b5b67706e71c954c474b99fcfc7
Leana <leanale003@gmail.com>
2024-12-05 01:05:22 -0800

    adding day to day to stables

--- stables.js
+++ stables.js
@@ -1,50 +1,44 @@
 
-⠀⠀⠀⠀⠀⠀⠀⠀⣀⣤⣶⣶⣿⣿⣿⣿⣷⣶⣤⣀⠀⠀⠀⠀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⢀⡴⠿⢿⣿⣿⡿⠿⠿⠛⠛⠿⠿⢿⣿⣿⣦⣤⠞⠉⠳⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⢀⣠⡶⠟⠋⠁⠀⠀⠀⠀⢀⠀⠠⠤⠤⠉⣻⠏⠀⠀⡄⠙⣆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⣠⣿⣭⠆⠀⠀⠀⠀⢀⠄⠊⠀⠀⠀⠀⠀⠀⢸⠀⠀⠀⢸⠀⢹⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⣩⡾⠁⠀⠀⠀⡠⠀⠁⠀⠀⣀⡤⠴⠒⠚⠛⠛⠀⠀⠀⡸⠀⢸⠀⠀⠀⢀⣴⡾⢷⣦⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⢤⣤⡶⠛⠁⠀⠀⡀⠐⠀⣀⣤⢀⣴⠾⢛⣓⣶⣇⣠⠆⠀⠀⠀⠀⠁⠀⡞⣴⠞⢦⡾⠋⠀⠀⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠈⠉⠛⠛⠒⠚⠒⣿⠉⢠⡷⡹⢁⣴⠟⠛⢻⣿⡧⠐⠀⠀⠀⠀⠀⣸⣽⠃⠀⠸⠁⠀⠀⣸⣃⣤⣤⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⢿⠀⠌⠀⡇⣼⣇⣀⣴⣿⣿⡇⠀⠀⠀⠀⢀⡜⠉⡏⠀⠀⠀⠀⠀⣰⠟⠉⠀⢹⡷⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⣾⠀⠀⠀⡅⣿⣿⣿⣿⣿⣿⠃⠀⠀⢀⡴⠋⠀⠀⡇⢀⣠⡀⠀⠔⠁⠀⠀⢀⡾⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⢰⡏⠅⠀⠀⠀⠐⢉⠡⣿⣷⠟⣁⣠⡤⠞⠋⠀⠀⠀⣰⡗⠉⢸⡇⠀⠀⠀⠀⣠⠟⠁⣀⣤⣶⣶⣶⣶⣶⣦⣤⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠈⠳⣄⣤⠇⠀⠀⠀⠈⠈⠀⠀⠈⢙⣲⠖⠀⠀⣠⣾⣿⠃⠀⣜⣤⡄⠀⣠⣾⣥⣤⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠁⠚⠷⢦⣤⣤⣀⣀⣤⣤⣴⠏⢀⣤⣶⣿⣿⡟⡿⠀⠜⠁⢀⡟⠈⠀⠀⣼⣿⠿⠛⣉⠀⠀⠀⡈⠉⠛⠿⣿⣿⣿⣿⣿⣆⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠀⣸⢳⢇⣴⣿⣿⣿⡿⠋⡼⠁⠀⠀⢀⡞⠁⣀⣴⣾⣟⣥⣶⣧⣤⣄⣐⠠⢀⠈⠂⢄⠈⠻⣿⣿⣟⢿⣧⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡟⢸⡿⢱⣿⣿⠏⠀⣼⠁⠀⠠⠶⣕⠺⣯⠁⠀⠀⠉⠻⣟⠛⠻⣿⣿⣷⡄⠑⠄⠀⠁⡀⠙⣿⣿⡄⠙⠇⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡇⠘⠀⣿⣿⠇⠀⣼⠇⠀⠀⣀⣠⣟⡴⠏⠐⢲⢂⠀⠀⠘⣧⠀⢸⣿⣿⣿⡄⠈⡄⠀⠐⡀⢹⣿⣧⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣤⠶⠖⠒⠒⠒⠿⠦⠤⠹⣿⠀⢰⣿⠀⠀⠀⠉⠉⠀⠀⠀⠌⢮⣿⡄⠀⠀⣿⠀⣼⣿⣿⣿⡇⠀⠀⠀⠀⠁⠀⣿⣿⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⢀⣴⠟⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⢧⣌⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⡿⡇⠀⠀⣿⣾⣿⣿⣿⣿⠇⠀⡀⠀⠀⠘⠀⣿⣿⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⢀⡴⠋⠀⠀⠀⠀⠀⠀⠀⣀⣀⣀⠀⠀⠀⣀⡠⠉⠛⠀⠀⠀⠀⠀⢠⠀⠀⠀⠸⢤⣴⣶⣿⣿⣿⣿⣿⣿⠏⠀⠀⠁⠀⠀⠀⠀⣿⣿⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⣴⠏⠀⠀⠀⠀⠀⠀⢀⡴⠛⠉⣹⠏⠉⠉⠉⠉⣷⠶⠤⣤⣀⣀⣀⣀⣈⣧⡀⠀⠀⠀⠈⠉⣿⢿⣿⣿⡿⠃⠀⠀⠈⠀⠀⠀⡇⠀⣿⣿⡇⠀⠀⠀⠀⠀⠀
-⠀⠀⠸⣧⣀⠀⠀⠀⠀⠀⣴⠟⠀⠀⢰⡏⠀⠀⠀⠀⣰⠃⠀⠀⠀⠀⠈⣿⠉⠉⠀⠙⣶⣄⠀⠀⠀⠀⣰⣿⠟⠀⠀⠀⠀⠃⠀⠀⠀⠀⠀⣿⣿⣷⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠉⠛⠓⠶⠶⠾⠃⠀⠀⠀⣿⠁⠀⠀⠀⠀⡟⠀⠀⠀⠀⠀⢀⡟⠀⠀⠀⠀⣿⠘⣇⠀⠀⢠⣿⠋⢀⣤⣦⠀⠀⠀⠀⠀⢰⠀⠀⢿⣿⣿⣧⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣿⠀⠀⠀⠀⠀⣧⠀⠀⠀⠀⠀⣾⠁⠀⠀⠀⠀⡟⢠⡇⠀⠀⢸⢃⣴⠟⠁⣿⠀⠀⠀⠀⠀⢸⠀⠀⠸⣏⠛⠿⢷⣄⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⣿⠀⠀⠀⠀⠀⢹⡄⠀⠀⠀⣸⠃⠀⠀⠀⠀⢸⡇⣾⠀⠀⠀⢸⡟⣽⠀⠀⢻⣇⠀⡀⠀⠀⠈⡄⠀⠀⢻⣆⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⠀⠀⠀⠀⠀⠈⢷⡀⠀⣰⠏⠀⠀⠀⠀⠀⡿⢰⠇⠀⠀⠀⠈⠁⣿⠀⠀⠀⢻⣦⣁⠀⠀⠀⠹⣦⡀⠀⠹⣷⣄⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢻⡄⠀⠀⠀⠀⣠⡿⠁⣰⠏⠀⠀⠀⠀⠀⣸⢃⡟⠀⠀⠀⠀⠀⢠⡏⠀⠀⠀⠀⠙⠻⣦⣄⡀⠀⠈⠻⣶⣤⣈⡙⠷⣦⣄⡀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⣧⠀⢀⣠⡾⠋⢀⣴⠋⠀⠀⠀⠀⢀⣴⠏⣼⠁⠀⠀⠀⠀⠀⣸⠇⠀⠀⠀⠀⠀⠀⠀⠉⠙⠛⠓⠶⠶⠿⠿⠿⠟⠉⠉⠉
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⠿⠛⠉⠀⠀⠈⠛⠛⠒⠚⠛⠛⠛⠁⠠⣯⣤⣤⣤⣤⣤⠶⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+//⠀⠀⠀⠀⠀⠀⠀⠀⣀⣤⣶⣶⣿⣿⣿⣿⣷⣶⣤⣀⠀⠀⠀⠀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+//⠀⠀⠀⠀⠀⢀⡴⠿⢿⣿⣿⡿⠿⠿⠛⠛⠿⠿⢿⣿⣿⣦⣤⠞⠉⠳⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+//⠀⠀⠀⠀⠀⢀⣠⡶⠟⠋⠁⠀⠀⠀⠀⢀⠀⠠⠤⠤⠉⣻⠏⠀⠀⡄⠙⣆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+//⠀⠀⠀⠀⣠⣿⣭⠆⠀⠀⠀⠀⢀⠄⠊⠀⠀⠀⠀⠀⠀⢸⠀⠀⠀⢸⠀⢹⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+//⠀⠀⠀⠀⣩⡾⠁⠀⠀⠀⡠⠀⠁⠀⠀⣀⡤⠴⠒⠚⠛⠛⠀⠀⠀⡸⠀⢸⠀⠀⠀⢀⣴⡾⢷⣦⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+//⢤⣤⡶⠛⠁⠀⠀⡀⠐⠀⣀⣤⢀⣴⠾⢛⣓⣶⣇⣠⠆⠀⠀⠀⠀⠁⠀⡞⣴⠞⢦⡾⠋⠀⠀⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+//⠀⠈⠉⠛⠛⠒⠚⠒⣿⠉⢠⡷⡹⢁⣴⠟⠛⢻⣿⡧⠐⠀⠀⠀⠀⠀⣸⣽⠃⠀⠸⠁⠀⠀⣸⣃⣤⣤⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+//⠀⠀⠀⠀⠀⠀⠀⠀⢿⠀⠌⠀⡇⣼⣇⣀⣴⣿⣿⡇⠀⠀⠀⠀⢀⡜⠉⡏⠀⠀⠀⠀⠀⣰⠟⠉⠀⢹⡷⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+//⠀⠀⠀⠀⠀⠀⠀⠀⣾⠀⠀⠀⡅⣿⣿⣿⣿⣿⣿⠃⠀⠀⢀⡴⠋⠀⠀⡇⢀⣠⡀⠀⠔⠁⠀⠀⢀⡾⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+//⠀⠀⠀⠀⠀⠀⢰⡏⠅⠀⠀⠀⠐⢉⠡⣿⣷⠟⣁⣠⡤⠞⠋⠀⠀⠀⣰⡗⠉⢸⡇⠀⠀⠀⠀⣠⠟⠁⣀⣤⣶⣶⣶⣶⣶⣦⣤⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+//⠀⠀⠀⠀⠀⠀⠈⠳⣄⣤⠇⠀⠀⠀⠈⠈⠀⠀⠈⢙⣲⠖⠀⠀⣠⣾⣿⠃⠀⣜⣤⡄⠀⣠⣾⣥⣤⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀
+//⠀⠀⠀⠀⠀⠀⠀⠀⠁⠚⠷⢦⣤⣤⣀⣀⣤⣤⣴⠏⢀⣤⣶⣿⣿⡟⡿⠀⠜⠁⢀⡟⠈⠀⠀⣼⣿⠿⠛⣉⠀⠀⠀⡈⠉⠛⠿⣿⣿⣿⣿⣿⣆⠀⠀⠀⠀⠀⠀⠀
+//⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠀⣸⢳⢇⣴⣿⣿⣿⡿⠋⡼⠁⠀⠀⢀⡞⠁⣀⣴⣾⣟⣥⣶⣧⣤⣄⣐⠠⢀⠈⠂⢄⠈⠻⣿⣿⣟⢿⣧⠀⠀⠀⠀⠀⠀
+//⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡟⢸⡿⢱⣿⣿⠏⠀⣼⠁⠀⠠⠶⣕⠺⣯⠁⠀⠀⠉⠻⣟⠛⠻⣿⣿⣷⡄⠑⠄⠀⠁⡀⠙⣿⣿⡄⠙⠇⠀⠀⠀⠀⠀
+//⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡇⠘⠀⣿⣿⠇⠀⣼⠇⠀⠀⣀⣠⣟⡴⠏⠐⢲⢂⠀⠀⠘⣧⠀⢸⣿⣿⣿⡄⠈⡄⠀⠐⡀⢹⣿⣧⠀⠀⠀⠀⠀⠀⠀
+//⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣤⠶⠖⠒⠒⠒⠿⠦⠤⠹⣿⠀⢰⣿⠀⠀⠀⠉⠉⠀⠀⠀⠌⢮⣿⡄⠀⠀⣿⠀⣼⣿⣿⣿⡇⠀⠀⠀⠀⠁⠀⣿⣿⠀⠀⠀⠀⠀⠀⠀
+//⠀⠀⠀⠀⠀⠀⢀⣴⠟⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⢧⣌⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⡿⡇⠀⠀⣿⣾⣿⣿⣿⣿⠇⠀⡀⠀⠀⠘⠀⣿⣿⠀⠀⠀⠀⠀⠀⠀
+//⠀⠀⠀⠀⢀⡴⠋⠀⠀⠀⠀⠀⠀⠀⣀⣀⣀⠀⠀⠀⣀⡠⠉⠛⠀⠀⠀⠀⠀⢠⠀⠀⠀⠸⢤⣴⣶⣿⣿⣿⣿⣿⣿⠏⠀⠀⠁⠀⠀⠀⠀⣿⣿⠀⠀⠀⠀⠀⠀⠀
+//⠀⠀⠀⣴⠏⠀⠀⠀⠀⠀⠀⢀⡴⠛⠉⣹⠏⠉⠉⠉⠉⣷⠶⠤⣤⣀⣀⣀⣀⣈⣧⡀⠀⠀⠀⠈⠉⣿⢿⣿⣿⡿⠃⠀⠀⠈⠀⠀⠀⡇⠀⣿⣿⡇⠀⠀⠀⠀⠀⠀
+//⠀⠀⠸⣧⣀⠀⠀⠀⠀⠀⣴⠟⠀⠀⢰⡏⠀⠀⠀⠀⣰⠃⠀⠀⠀⠀⠈⣿⠉⠉⠀⠙⣶⣄⠀⠀⠀⠀⣰⣿⠟⠀⠀⠀⠀⠃⠀⠀⠀⠀⠀⣿⣿⣷⠀⠀⠀⠀⠀⠀
+//⠀⠀⠀⠀⠉⠛⠓⠶⠶⠾⠃⠀⠀⠀⣿⠁⠀⠀⠀⠀⡟⠀⠀⠀⠀⠀⢀⡟⠀⠀⠀⠀⣿⠘⣇⠀⠀⢠⣿⠋⢀⣤⣦⠀⠀⠀⠀⠀⢰⠀⠀⢿⣿⣿⣧⠀⠀⠀⠀⠀
+//⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣿⠀⠀⠀⠀⠀⣧⠀⠀⠀⠀⠀⣾⠁⠀⠀⠀⠀⡟⢠⡇⠀⠀⢸⢃⣴⠟⠁⣿⠀⠀⠀⠀⠀⢸⠀⠀⠸⣏⠛⠿⢷⣄⠀⠀⠀
+//⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⣿⠀⠀⠀⠀⠀⢹⡄⠀⠀⠀⣸⠃⠀⠀⠀⠀⢸⡇⣾⠀⠀⠀⢸⡟⣽⠀⠀⢻⣇⠀⡀⠀⠀⠈⡄⠀⠀⢻⣆⠀⠀⠀⠀⠀⠀
+//⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⠀⠀⠀⠀⠀⠈⢷⡀⠀⣰⠏⠀⠀⠀⠀⠀⡿⢰⠇⠀⠀⠀⠈⠁⣿⠀⠀⠀⢻⣦⣁⠀⠀⠀⠹⣦⡀⠀⠹⣷⣄⠀⠀⠀⠀
+//⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢻⡄⠀⠀⠀⠀⣠⡿⠁⣰⠏⠀⠀⠀⠀⠀⣸⢃⡟⠀⠀⠀⠀⠀⢠⡏⠀⠀⠀⠀⠙⠻⣦⣄⡀⠀⠈⠻⣶⣤⣈⡙⠷⣦⣄⡀
+//⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⣧⠀⢀⣠⡾⠋⢀⣴⠋⠀⠀⠀⠀⢀⣴⠏⣼⠁⠀⠀⠀⠀⠀⣸⠇⠀⠀⠀⠀⠀⠀⠀⠉⠙⠛⠓⠶⠶⠿⠿⠿⠟⠉⠉⠉
+//⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⠿⠛⠉⠀⠀⠈⠛⠛⠒⠚⠛⠛⠛⠁⠠⣯⣤⣤⣤⣤⣤⠶⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
 
 //------------------------- Seting up shop -------------------------//
 
 let horses = [];
-// Create a variable and use it to store a message visitors to the stable.
 let visitorMessage = "WELCOME TO THE STABLE OF DOOM!!!!!!!!!!"
-// Create a variable and use it to store the cost of a late payment fee.
 let stableMonthlyFee = 100;    
 let lateFee = 100;            
-
-//let stableLateFee = (STABLE_MONTTHLY_FEE * months) + LATE_FEE;
-
-// Create a variable and use it to store the number of available stalls in the stable.
-
 let availStalls = 12;
 
 //------------------------- First day -------------------------//
 
-function Horse (name, nickname, favTreat, age, monthlyRent, isInside, color, iq) {
+// constructor for horses 
+function Horse (name, nickname, favTreat, age, monthlyRent, isInside, color, iq) {    
     this.name = name;
     this.nickname = nickname;
     this.favTreat = favTreat;
@@ -70,11 +64,11 @@ horses.push(hippo);
 horses.push(beans);
 horses.push(beef);
 
-// Fancy! Another horse wants to be stabled! Create a variable that stores an object
 
-let mushy = {
+// New horse as object literal
+let mushy = {        
     name: "Mushy",
-    nickName: "Peas",
+    nickname: "Peas",
     favTreat: "Dirt", 
     age: 5,
     monthlyRent: 210, 
@@ -156,8 +150,6 @@ horses[3]["isHungry"] = false;
 
 //---------------------- Growing business ----------------------//
 
-// We've added some horses! Re-assign the variable that contains the number of available stalls.
-
 availStalls = availStalls - horses.length;
 
 if (availStalls < 2) {
@@ -166,51 +158,98 @@ if (availStalls < 2) {
     console.log("We have " + availStalls + " stalls available!")
 }
 
-// Create a function that logs out how much will an individual horse will owe if rent is
-// paid late. Invoke the function.
+function rentCostFees(horse) {
 
-function rentCost() {
-    let outputMessage;
-
-    if (password === input) {
-        outputMessage = "Access Granted!";
-
-        if(password === "forgot" || "reset"){
-            outputMessage += "This password should not be used because it glitches the system...";
-        }
-    } else {
-        outputMessage = "Access Denied!";
-    }
-
-    }
-    console.log(outputMessage);
+    let rentLateFee = horse.monthlyRent + lateFee;
+    console.log(`${horse.name} would owe $${rentLateFee} if rent is paid late but they are a horse so they can pay in carrots`);
+    
 }
+
+rentCostFees(horses[0]);
 
 // Add loop that checks to find out if a certain horse likes a treat. If the horse does not like
 // the treat, log out their disatisfaction and check the next horse. Exit the loop when you find
 // the horse that likes the treat.
 
-// Create and invoke a function that returns (not logs) the nickname of a chosen horse. Log out
-// the return value outside of the function.
+function horseTreat(horses, treat) {
 
+    for (let i = 0; i < horses.length; i++) {
+        if (horses[i].favTreat === treat) {
+            console.log(`${horses[i].name} is feasting!! yummy`);
+            break;    //exit the loop
+        }else {
+            console.log(`${horses[i].name} is very disappointed...`)
+        }
+    }
+}
+
+horseTreat(horses, "Dirt");
+
+function horseChosenNickname (horse) {    
+
+    return horse.nickname;
+
+}
+
+let chosenHorse = horses[1]; //Soup (thats the name of the selected horse)
+console.log(`${chosenHorse.name} also goes by ${horseChosenNickname(chosenHorse)}`)
 //------------------------- Day to day operations -------------------------//
 
-// Create and invoke a function references one of your unique horse properties. It should
-// contain a conditional or a loop.
+function horseSmartOrNot(horse) {
+
+    if (horse.iq < 100){
+        console.log(`${horse.name} is missing some braincells`)
+    } else {
+        console.log(`WOW! ${horse.name} is soooooo smart!`)
+    }
+}
+
+horseSmartOrNot(horses[2]);
 
 // Morning! Create and invoke a function that moves all your horses outside. It should
 // log out statement indicating the horses have been moved to spend time in the sun.
 
+function moveHorsesOutside (horses) {
+
+    horses.isInside = false;
+    console.log(`what are they doing outside??? To spend time in the sun?? the world is ending!`)
+
+}
+
+moveHorsesOutside(horses);
+
 // Initialize a method on your horses that moves them inside if they are outside, and
 // vice versa.
+
+for (let i = 0; i < horses.length; i++) {
+    if (!horses[i]["isInside"]){
+        horses.isInside = true;
+    } else {
+        horses.isInside = false;
+    }
+}
+
+
 
 // Food! Create and invoke a function that feeds your horses. If the horse is outside, it
 // should call them in, and then all horses should be fed a treat. Log the activity of
 // the horses.
 
+for (let i = 0; i < horses.length; i++) {
+    let message = horses[i]["name"];
+
+    if(!horses[i]["isInside"]) {
+        message += ", come inside!!";
+    }
+
+    message += "! Have some " + horses[i]["favTreat"] + "!";
+    console.log(message);
+
+}
+
+
 // Bedtime! Update the function that lets your horses outside so that it also calls them
 // in if it is getting dark. Only the horses that moved should log out a message. Invoke the function.
 
-//------------------------- Bonus -------------------------//
 
-// Replace the ascii art at the top of the file with art representing your stable.
+
