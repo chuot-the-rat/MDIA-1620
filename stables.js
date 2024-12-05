f2a3f9fdc81842b99b80989ea444d8d0ce9f2cbf
Leana <leanale003@gmail.com>
2024-12-05 01:58:12 -0800

    add code from branch to main

--- stables.js
+++ stables.js
@@ -1,27 +1,265 @@
 
-console.log("horse! horse! horse! horse! horse!horse!horse!horse!horse!horse!")
+//⠀⠀⠀⠀⠀⠀⠀⠀⣀⣤⣶⣶⣿⣿⣿⣿⣷⣶⣤⣀⠀⠀⠀⠀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+//⠀⠀⠀⠀⠀⢀⡴⠿⢿⣿⣿⡿⠿⠿⠛⠛⠿⠿⢿⣿⣿⣦⣤⠞⠉⠳⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+//⠀⠀⠀⠀⠀⢀⣠⡶⠟⠋⠁⠀⠀⠀⠀⢀⠀⠠⠤⠤⠉⣻⠏⠀⠀⡄⠙⣆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+//⠀⠀⠀⠀⣠⣿⣭⠆⠀⠀⠀⠀⢀⠄⠊⠀⠀⠀⠀⠀⠀⢸⠀⠀⠀⢸⠀⢹⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+//⠀⠀⠀⠀⣩⡾⠁⠀⠀⠀⡠⠀⠁⠀⠀⣀⡤⠴⠒⠚⠛⠛⠀⠀⠀⡸⠀⢸⠀⠀⠀⢀⣴⡾⢷⣦⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+//⢤⣤⡶⠛⠁⠀⠀⡀⠐⠀⣀⣤⢀⣴⠾⢛⣓⣶⣇⣠⠆⠀⠀⠀⠀⠁⠀⡞⣴⠞⢦⡾⠋⠀⠀⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+//⠀⠈⠉⠛⠛⠒⠚⠒⣿⠉⢠⡷⡹⢁⣴⠟⠛⢻⣿⡧⠐⠀⠀⠀⠀⠀⣸⣽⠃⠀⠸⠁⠀⠀⣸⣃⣤⣤⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+//⠀⠀⠀⠀⠀⠀⠀⠀⢿⠀⠌⠀⡇⣼⣇⣀⣴⣿⣿⡇⠀⠀⠀⠀⢀⡜⠉⡏⠀⠀⠀⠀⠀⣰⠟⠉⠀⢹⡷⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+//⠀⠀⠀⠀⠀⠀⠀⠀⣾⠀⠀⠀⡅⣿⣿⣿⣿⣿⣿⠃⠀⠀⢀⡴⠋⠀⠀⡇⢀⣠⡀⠀⠔⠁⠀⠀⢀⡾⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+//⠀⠀⠀⠀⠀⠀⢰⡏⠅⠀⠀⠀⠐⢉⠡⣿⣷⠟⣁⣠⡤⠞⠋⠀⠀⠀⣰⡗⠉⢸⡇⠀⠀⠀⠀⣠⠟⠁⣀⣤⣶⣶⣶⣶⣶⣦⣤⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+//⠀⠀⠀⠀⠀⠀⠈⠳⣄⣤⠇⠀⠀⠀⠈⠈⠀⠀⠈⢙⣲⠖⠀⠀⣠⣾⣿⠃⠀⣜⣤⡄⠀⣠⣾⣥⣤⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀
+//⠀⠀⠀⠀⠀⠀⠀⠀⠁⠚⠷⢦⣤⣤⣀⣀⣤⣤⣴⠏⢀⣤⣶⣿⣿⡟⡿⠀⠜⠁⢀⡟⠈⠀⠀⣼⣿⠿⠛⣉⠀⠀⠀⡈⠉⠛⠿⣿⣿⣿⣿⣿⣆⠀⠀⠀⠀⠀⠀⠀
+//⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠀⣸⢳⢇⣴⣿⣿⣿⡿⠋⡼⠁⠀⠀⢀⡞⠁⣀⣴⣾⣟⣥⣶⣧⣤⣄⣐⠠⢀⠈⠂⢄⠈⠻⣿⣿⣟⢿⣧⠀⠀⠀⠀⠀⠀
+//⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡟⢸⡿⢱⣿⣿⠏⠀⣼⠁⠀⠠⠶⣕⠺⣯⠁⠀⠀⠉⠻⣟⠛⠻⣿⣿⣷⡄⠑⠄⠀⠁⡀⠙⣿⣿⡄⠙⠇⠀⠀⠀⠀⠀
+//⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡇⠘⠀⣿⣿⠇⠀⣼⠇⠀⠀⣀⣠⣟⡴⠏⠐⢲⢂⠀⠀⠘⣧⠀⢸⣿⣿⣿⡄⠈⡄⠀⠐⡀⢹⣿⣧⠀⠀⠀⠀⠀⠀⠀
+//⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣤⠶⠖⠒⠒⠒⠿⠦⠤⠹⣿⠀⢰⣿⠀⠀⠀⠉⠉⠀⠀⠀⠌⢮⣿⡄⠀⠀⣿⠀⣼⣿⣿⣿⡇⠀⠀⠀⠀⠁⠀⣿⣿⠀⠀⠀⠀⠀⠀⠀
+//⠀⠀⠀⠀⠀⠀⢀⣴⠟⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⢧⣌⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⡿⡇⠀⠀⣿⣾⣿⣿⣿⣿⠇⠀⡀⠀⠀⠘⠀⣿⣿⠀⠀⠀⠀⠀⠀⠀
+//⠀⠀⠀⠀⢀⡴⠋⠀⠀⠀⠀⠀⠀⠀⣀⣀⣀⠀⠀⠀⣀⡠⠉⠛⠀⠀⠀⠀⠀⢠⠀⠀⠀⠸⢤⣴⣶⣿⣿⣿⣿⣿⣿⠏⠀⠀⠁⠀⠀⠀⠀⣿⣿⠀⠀⠀⠀⠀⠀⠀
+//⠀⠀⠀⣴⠏⠀⠀⠀⠀⠀⠀⢀⡴⠛⠉⣹⠏⠉⠉⠉⠉⣷⠶⠤⣤⣀⣀⣀⣀⣈⣧⡀⠀⠀⠀⠈⠉⣿⢿⣿⣿⡿⠃⠀⠀⠈⠀⠀⠀⡇⠀⣿⣿⡇⠀⠀⠀⠀⠀⠀
+//⠀⠀⠸⣧⣀⠀⠀⠀⠀⠀⣴⠟⠀⠀⢰⡏⠀⠀⠀⠀⣰⠃⠀⠀⠀⠀⠈⣿⠉⠉⠀⠙⣶⣄⠀⠀⠀⠀⣰⣿⠟⠀⠀⠀⠀⠃⠀⠀⠀⠀⠀⣿⣿⣷⠀⠀⠀⠀⠀⠀
+//⠀⠀⠀⠀⠉⠛⠓⠶⠶⠾⠃⠀⠀⠀⣿⠁⠀⠀⠀⠀⡟⠀⠀⠀⠀⠀⢀⡟⠀⠀⠀⠀⣿⠘⣇⠀⠀⢠⣿⠋⢀⣤⣦⠀⠀⠀⠀⠀⢰⠀⠀⢿⣿⣿⣧⠀⠀⠀⠀⠀
+//⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣿⠀⠀⠀⠀⠀⣧⠀⠀⠀⠀⠀⣾⠁⠀⠀⠀⠀⡟⢠⡇⠀⠀⢸⢃⣴⠟⠁⣿⠀⠀⠀⠀⠀⢸⠀⠀⠸⣏⠛⠿⢷⣄⠀⠀⠀
+//⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⣿⠀⠀⠀⠀⠀⢹⡄⠀⠀⠀⣸⠃⠀⠀⠀⠀⢸⡇⣾⠀⠀⠀⢸⡟⣽⠀⠀⢻⣇⠀⡀⠀⠀⠈⡄⠀⠀⢻⣆⠀⠀⠀⠀⠀⠀
+//⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⠀⠀⠀⠀⠀⠈⢷⡀⠀⣰⠏⠀⠀⠀⠀⠀⡿⢰⠇⠀⠀⠀⠈⠁⣿⠀⠀⠀⢻⣦⣁⠀⠀⠀⠹⣦⡀⠀⠹⣷⣄⠀⠀⠀⠀
+//⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢻⡄⠀⠀⠀⠀⣠⡿⠁⣰⠏⠀⠀⠀⠀⠀⣸⢃⡟⠀⠀⠀⠀⠀⢠⡏⠀⠀⠀⠀⠙⠻⣦⣄⡀⠀⠈⠻⣶⣤⣈⡙⠷⣦⣄⡀
+//⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⣧⠀⢀⣠⡾⠋⢀⣴⠋⠀⠀⠀⠀⢀⣴⠏⣼⠁⠀⠀⠀⠀⠀⣸⠇⠀⠀⠀⠀⠀⠀⠀⠉⠙⠛⠓⠶⠶⠿⠿⠿⠟⠉⠉⠉
+//⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⠿⠛⠉⠀⠀⠈⠛⠛⠒⠚⠛⠛⠛⠁⠠⣯⣤⣤⣤⣤⣤⠶⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
 
-/* 
-name variables using CAMEL CASE 
-A way to separate the words 
-First letter of each word capitalized no spaces
-*/
 
-let horseName = "SomeCoolGuy";          //this is a string, for string prefer double quotes " "
-horseName = "Chicken";
-let horseAge = 5;
 
-console.log(horseName);
-console.log(horseAge);
-
-let isHorseInStable = false;            // always name boolean in a way thats readable to people
-
-console.log(isHorseInStable);
-
-isHorseInStable = false;
-horseName = "Hippo"                     // still chicken ^ because it already console.log out before this line 
-
-console.log(horseName);
+let horses = [];
+let visitorMessage = "WELCOME TO THE STABLE OF DOOM!!!!!!!!!!"
+let stableMonthlyFee = 100;    
+let lateFee = 100;            
+let availStalls = 12;
 
 
 
+// constructor for horses 
+function Horse (name, nickname, favTreat, age, monthlyRent, isInside, color, iq) {    
+    this.name = name;
+    this.nickname = nickname;
+    this.favTreat = favTreat;
+    this.age = age;
+    this.monthlyRent = monthlyRent;
+    this.isInside = isInside;
+    this.color = color;
+    this.iq = iq;
+    this.isHungry = false;
+    this.introduction = function() {
+        return(`Welcome! This is ${this.name}! Their nickname is ${this.nickname}! They are ${this.age} years old`);
+    };
+    this.iqNotice = function() {
+        return(`${this.name}'s not so bright... their iq is only ${this.iq}`);
+    };
+}
+
+
+
+let hippo = new Horse("Hippo", "Cow", "bees", 1, 100, true, "pink", 13);
+let beans = new Horse("Soup", "Cream of Mushroom", "Grass", 2, 90000, false, "blue", 50);
+let beef = new Horse("Beef", "Roast", "Souls", 10, 75, true, "green", 1); 
+
+horses.push(hippo);
+horses.push(beans);
+horses.push(beef);
+
+
+
+// New horse as object literal
+let mushy = {        
+    name: "Mushy",
+    nickname: "Peas",
+    favTreat: "Dirt", 
+    age: 5,
+    monthlyRent: 210, 
+    isInside: false,
+    color: "Multicolor",
+    iq: 1000,
+    isHungry: false,
+    introduction() {
+        return(`Welcome! This is ${this.name}! Their nickname is ${this.nickname}! They are ${this.age} years old`);
+    },
+    iqNotice() {
+        return(`${this.name}'s not so bright... their iq is only ${this.iq}`);
+    }
+};
+
+horses.push(mushy);
+
+horses[0]["isHungry"] = true; 
+horses[1]["isHungry"] = true; 
+horses[2]["isHungry"] = false; 
+
+
+
+//[
+//    Horse {
+//      name: 'Hippo',
+//      nickname: 'Cow',
+//      favTreat: 'bees',
+//      age: 1,
+//      monthlyRent: 100,
+//      isInside: true,
+//      color: 'pink',
+//      iq: 13,
+//      isHungry: true,
+//      introduction: [Function (anonymous)],
+//      iqNotice: [Function (anonymous)]
+//    },
+//    Horse {
+//      name: 'Soup',
+//      nickname: 'Cream of Mushroom',
+//      favTreat: 'Grass',
+//      age: 2,
+//      monthlyRent: 90000,
+//      isInside: false,
+//      color: 'blue',
+//      iq: 50,
+//      isHungry: true,
+//      introduction: [Function (anonymous)],
+//      iqNotice: [Function (anonymous)]
+//    },
+//    Horse {
+//      name: 'Beef',
+//      nickname: 'Roast',
+//      favTreat: 'Souls',
+//      age: 10,
+//      monthlyRent: 75,
+//      isInside: true,
+//      color: 'green',
+//      iq: 1,
+//      isHungry: false,
+//      introduction: [Function (anonymous)],
+//      iqNotice: [Function (anonymous)]
+//    },
+//    {
+//      name: 'Mushy',
+//      nickName: 'Peas',
+//      favTreat: 'Dirt',
+//      age: 5,
+//      monthlyRent: 210,
+//      isInside: false,
+//      color: 'Multicolor',
+//      iq: 1000,
+//      isHungry: false,
+//      introduction: [Function: introduction],
+//      iqNotice: [Function: iqNotice]
+//    }
+//  ]
+
+
+
+availStalls = availStalls - horses.length;
+
+if (availStalls < 2) {
+    console.log("We need to build more stalls")
+} else {
+    console.log("We have " + availStalls + " stalls available!")
+}
+
+function rentCostFees(horse) {
+
+    let rentLateFee = horse.monthlyRent + lateFee;
+    console.log(`${horse.name} would owe $${rentLateFee} if rent is paid late but they are a horse so they can pay in carrots`);
+    
+}
+
+rentCostFees(horses[0]);
+
+
+
+function horseTreat(horses, treat) {
+
+    for (let i = 0; i < horses.length; i++) {
+        if (horses[i].favTreat === treat) {
+            console.log(`${horses[i].name} is feasting!! yummy`);
+            break;    //exit the loop
+        }else {
+            console.log(`${horses[i].name} is very disappointed...`)
+        }
+    }
+}
+
+horseTreat(horses, "Dirt");
+
+
+function horseChosenNickname (horse) {    
+
+    return horse.nickname;
+
+}
+
+let chosenHorse = horses[1]; //Soup (thats the name of the selected horse)
+console.log(`${chosenHorse.name} also goes by ${horseChosenNickname(chosenHorse)}`)
+
+
+
+function horseSmartOrNot(horse) {
+
+    if (horse.iq < 100){
+        console.log(`${horse.name} is missing some braincells`)
+    } else {
+        console.log(`WOW! ${horse.name} is soooooo smart!`)
+    }
+}
+
+horseSmartOrNot(horses[2]);
+
+
+
+function moveHorsesOutside (horses) {
+
+    for (let i = 0; i < horses.length; i++) {
+        horses[i].isInside = false; // Move each horse outside
+        console.log(`${horses[i].name}? what are they doing outside??? To spend time in the sun?? the world is ending!`);
+    }
+}
+
+moveHorsesOutside(horses);
+
+
+function moveInsideOutside(horse) {
+    horse.isInside = !horse.isInside; 
+    return horse.isInside;
+}
+
+
+function feedHorses(horses) {
+    for (let i = 0; i < horses.length; i++) {
+        if (!horses[i].isInside) {
+            horses[i].isInside = true; 
+            console.log(`${horses[i].name} was outside and now came in`);
+        }
+        console.log(`${horses[i].name} is munching on ${horses[i].favTreat}!`);
+    }
+}
+
+
+for (let i = 0; i < horses.length; i++) {
+    let message = horses[i]["name"];
+
+    if(!horses[i]["isInside"]) {
+        message += ", come inside!!";
+    }
+
+    message += "! Have some " + horses[i]["favTreat"] + "!";
+    console.log(message);
+
+}
+
+function bedtime(horses, night) {
+    for (let i = 0; i < horses.length; i++){
+        if (night && !horses[i].isInside) {
+            horses[i].isInside = true; //move horse inside
+            console.log(`${horses[i].name} came inside zzz`);
+        } else if (!night && horses[i].isInside){
+            horses[i].isInside = false; //move horse outside
+            console.log(`${horses[i].name} wants to see the world burn still`);
+        }
+    }
+}
+
+bedtime(horses, false);
